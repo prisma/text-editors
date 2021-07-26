34e1498222d286f87f7dcc50af4135f07ae6bdaf
Siddhant <me@madebysid.com>
2021-07-26 17:44:19 +0200

    Add a `lineAndColumnFromPos` utility

---
+++ src/hooks/useEditor/lineAndColumnFromPos.ts
@@ -0,0 +1,9 @@
+import { EditorState } from "@codemirror/state";
+
+/** Takes a (CodeMirror) editor position and returns the line and column number */
+export function lineAndColumnFromPos(state: EditorState, pos: number) {
+  const line = state.doc.lineAt(pos);
+  const column = pos - line.from;
+
+  return { line, column };
+}
