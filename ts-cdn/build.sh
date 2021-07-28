08a5ca27e6e3a546e3b74f62f30700bcaabf7708
Siddhant <me@madebysid.com>
2021-07-28 18:05:20 +0200

    cdn by version

--- ts-cdn/build.sh
+++ ts-cdn/build.sh
@@ -2,5 +2,8 @@
 
 rm -rf public/typescript
 
-mkdir -p public/typescript/lib
-cp node_modules/typescript/lib/*.d.ts public/typescript/lib
+version=$(cat package.json | grep typescript | awk -F '"' '{ print $4 }')
+echo Using TypeScript version: $version
+
+mkdir -p public/typescript/$version/lib
+cp node_modules/typescript/lib/*.d.ts public/typescript/$version/lib
