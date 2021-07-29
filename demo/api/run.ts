093cb26bc6b84b8e11a6afe369a9b36069aeead4
Siddhant <me@madebysid.com>
2021-07-29 14:43:10 +0200

    refactor(demo): remove studio-pcw

--- demo/api/run.ts
+++ demo/api/run.ts
@@ -1,14 +1,10 @@
-import * as fs from "fs";
-import * as path from "path";
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { PCW, parseDatasourceUrl } from "@prisma/studio-pcw";
+import { PrismaClient } from "@prisma/client";
 
 type RequestBody = {
   schema: string;
   query: string;
 };
-
-const DB_URL = process.env.DB_URL as string;
 
 export default async function types(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
@@ -16,22 +12,14 @@ export default async function types(req: VercelRequest, res: VercelResponse) {
   }
 
   const { query } = req.body as RequestBody;
-  const schemaPath = path.resolve(
-    "../node_modules/.prisma/client/schema.prisma"
-  );
-  const schema = fs.readFileSync(schemaPath, "utf-8");
-  const { env } = parseDatasourceUrl(schema);
 
-  const pcw = new PCW(
-    schema,
-    schemaPath,
-    {
-      [`${env}`]: DB_URL,
-    },
-    {
-      forcePrismaLibrary: true,
-    }
-  );
+  const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
 
-  res.json({ query, response: await pcw.request(query) });
+  const prisma = new PrismaClient();
+  const response = await new AsyncFunction("prisma", `return await ${query}`)(
+    prisma
+  );
+  prisma.$disconnect();
+
+  return res.json({ query, response });
 }
