7e0311ee00b14fe5ed17b91bd337cd5457d35287
Siddhant <me@madebysid.com>
2021-07-29 15:00:48 +0200

    fix(demo): use CDN for types

--- demo/build.sh
+++ demo/build.sh
@@ -5,3 +5,9 @@ cd ../
 yarn
 yarn build
 cp -R dist ./demo/public
+
+# Make Prisma Client types accessible to the CDN
+cd demo
+rm -rf public/types
+mkdir -p public/types
+cp node_modules/.prisma/client/index.d.ts public/types/prisma-client.d.ts
