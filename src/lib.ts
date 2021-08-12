3392f03f38f63646537cfb9c5710052c967a0eed
Siddhant <me@madebysid.com>
2021-08-12 03:34:51 +0200

    refactor: components -> react

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,5 @@
-export { Editor as JSONEditor } from "./components/JSONEditor";
-export { Editor as PrismaSchemaEditor } from "./components/PrismaSchemaEditor";
-export { Editor as SQLEditor } from "./components/SQLEditor";
-export { Editor as TSEditor } from "./components/TSEditor";
 export type { FileMap, SQLDialect, ThemeName } from "./editor";
+export { Editor as JSONEditor } from "./react/JSONEditor";
+export { Editor as PrismaSchemaEditor } from "./react/PrismaSchemaEditor";
+export { Editor as SQLEditor } from "./react/SQLEditor";
+export { Editor as TSEditor } from "./react/TSEditor";
